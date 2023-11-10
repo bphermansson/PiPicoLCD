f92cd5948e46b9dc0a85efbbbd310aa78a9167b6
Patrik Hermansson <hermansson.patrik@gmail.com>
2023-11-10 18:22:34 +0100

    Added info

--- 3_5_display_test.ino
+++ 3_5_display_test.ino
@@ -1,3 +1,14 @@
+/*
+A test project to drive a 3.5" LCD with a Raspberry Pi Pico W. 
+This project uses Bodmer's LCD library, https://github.com/Bodmer/TFT_eSPI
+and a Noname LCD, http://www.lcdwiki.com/3.5inch_Arduino_Display-Mega2560
+I can not recommend this display as you have to connect it with 16 bits parallell cables,
+this means over 20 cables between the display and the Pico when power and control pins are added. 
+
+Many fonts can be found in the librarys example folder, especially in "Smooth Fonts". 
+Download the fonts file and save it on the Pico, then refer to it below. 
+*/
+
 #include <TFT_eSPI.h> // Hardware-specific library
 #include "NotoSansBold36.h"
 #include <WiFi.h>
@@ -30,10 +41,10 @@ void setup() {
   // We can now plot text on screen using the "print" class
   tft.println("Hello World!");
 
-  tft.setTextSize(3);   
+  tft.setTextSize(2);   
   tft.setTextColor(TFT_YELLOW);  // Set text colour to black, no background (so transparent)
   tft.setCursor(76, 150, 4);    // Set cursor to x = 76, y = 150 and use font 4
-  tft.println("TEST");  
+  tft.println("Connecting to WiFi");  
 
   // We start by connecting to a WiFi network
   Serial.begin(115200);
